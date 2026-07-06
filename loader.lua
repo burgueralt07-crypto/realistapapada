ff518649195ad0bcec244e2629e48718706b2217
burgueralt07-crypto <burgueralt07@gmail.com>
2026-07-06 13:21:31 -0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,10 +1,8 @@
 -- loader.lua
-local function download(path)
-    local url = "https://raw.githubusercontent.com/burgueralt07-crypto/realistapapada/main/" .. path
-    local suc, res = pcall(function() return game:HttpGet(url) end)
-    if suc then return res else error("Falha ao baixar: " .. path) end
+local function getUrl(path)
+    return "https://raw.githubusercontent.com/burgueralt07-crypto/realistapapada/main/" .. path
 end
 
--- Salva e executa o main do Vape
-writefile("newvape/main.lua", download("newvape/main.lua"))
-loadstring(readfile("newvape/main.lua"))()
+-- Baixa o main e executa
+local mainScript = game:HttpGet(getUrl("newvape/main.lua"))
+loadstring(mainScript)()
